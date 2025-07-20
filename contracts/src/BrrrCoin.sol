6ffd16b04ab8a9e56d7dedcaab778679985993df
Rudransh Pratap Singh <tosylfluoride@gmail.com>
2025-07-20 10:02:38 +0530

    feat: implement transfer failure handling in BrrrCoin contract

--- contracts/src/BrrrCoin.sol
+++ contracts/src/BrrrCoin.sol
@@ -15,4 +15,14 @@ contract BrrrCoin is ERC20, Ownable {
     function setFailTranfers(bool _fail) external onlyOwner() {
         failTransfers = _fail;
     }
+
+    function transfer(address _to, uint256 _amount) public override returns(bool) {
+        if(failTransfers) revert BridgeMockToken__TransferFailed();
+        return super.transfer(_to, _amount);
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public override returns(bool) {
+        if(failTransfers) revert BridgeMockToken__TransferFailed();
+        return super.transferFrom(_from, _to, _value);
+    }
 }
